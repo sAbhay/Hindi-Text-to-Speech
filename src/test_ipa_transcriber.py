beed1f5396ea9e62f534c5e68ce5b584f4120729
Abhay Singhal <abhayaaditsinghal@gmail.com>
2021-08-08 03:46:07 -0700

    added virama tests

--- src/test_ipa_transcriber.py
+++ src/test_ipa_transcriber.py
@@ -7,3 +7,17 @@ def test_transcribe_viramas():
     names = [unicodedata.name(c) for c in text]
     actual = t.transcribe_viramas(names)
     assert actual == expected
+
+def test_transcribe_viramas_sequential():
+    text = "द्रक्ष"
+    expected = ['d̪r', 'kʂ']
+    names = [unicodedata.name(c) for c in text]
+    actual = t.transcribe_viramas(names)
+    assert actual == expected
+
+def test_transcribe_viramas_multi_cluster():
+    text = "चन्द्र"
+    expected = ['DEVANAGARI LETTER CA', "nd̪r"]
+    names = [unicodedata.name(c) for c in text]
+    actual = t.transcribe_viramas(names)
+    assert actual == expected
